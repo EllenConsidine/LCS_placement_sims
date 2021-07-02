819047745a01151463f020608b2eac714515bd8e
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2021-07-02 09:44:03 -0400

    Add files via upload

---
+++ Analysis/Merging_results.R
@@ -0,0 +1,56 @@
+library(stringr)
+
+setwd("C:/Users/ellen/OneDrive/MyDocs/Graduate Research/Low-cost AQ sensor epi/Analysis/Results")
+
+Metrics<- c(rep(c("MAE", "RMSE"),3),
+            rep(c("Real class > shown class","Shown class > real class",
+                  "AQI class off by >1"),3),
+            rep(c("95th percentile of errors",
+                  "% of the |errors| > 10"),3) )
+prefix<- "D1-"
+suffix<- "_avg.csv"
+
+pre_files<- list.files("C:/Users/ellen/OneDrive/MyDocs/Graduate Research/Low-cost AQ sensor epi/Analysis/Results",
+           prefix)
+suff_files<- list.files("C:/Users/ellen/OneDrive/MyDocs/Graduate Research/Low-cost AQ sensor epi/Analysis/Results",
+                        suffix)
+files<- intersect(pre_files, suff_files)
+
+Names<- sapply(files, function(x) str_split(x, "-")[[1]][2])
+Names<- sapply(names, function(x) str_split(x, "0_")[[1]][1])
+Names<- sapply(Names, function(x) paste0(x,"0"))
+
+# Get unweighted
+
+unweighted<- str_detect(files, "unw")
+
+UNW<- matrix(0, nrow=21, ncol=25)
+
+for(i in 1:25){
+  results<- read.csv(files[unweighted][i])
+  UNW[,i]<- t(results)
+}
+
+colnames(UNW)<- Names[unweighted]
+row.names(UNW)<- Metrics
+
+write.csv(UNW, "Results_12_days_unweighted.csv")
+
+
+# Get weighted
+
+weighted<- !unweighted
+
+W<- matrix(0, nrow=21, ncol=25)
+
+for(i in 1:25){
+  results<- read.csv(files[weighted][i])
+  W[,i]<- t(results)
+}
+
+colnames(W)<- Names[weighted]
+row.names(W)<- Metrics
+
+write.csv(W, "Results_12_days_weighted.csv")
+
+
