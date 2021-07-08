59a7f7b924ae970b7bd054c16025d43f75355dda
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2021-07-08 08:23:19 -0400

    Including distance / NN metrics

--- Analysis/Merging_results.R
+++ Analysis/Merging_results.R
@@ -6,7 +6,13 @@ Metrics<- c(rep(c("MAE", "RMSE"),3),
             rep(c("Real class > shown class","Shown class > real class",
                   "AQI class off by >1"),3),
             rep(c("95th percentile of errors",
-                  "% of the |errors| > 10"),3) )
+                  "% of the |errors| > 10"),3),
+           rep(c("Mean distance to NN monitor/sensor"),3),
+            rep(c("Mean distance to NN monitor/sensor, among misclassifications > 1"),3),
+            rep(c("Median distance to NN monitor/sensor"),3),
+            rep(c("Median distance to NN monitor/sensor, among misclassifications > 1"),3),
+            rep(c("Mean % of NNs that are LCSs"),3),
+            rep(c("Mean % of NNs that are LCSs, among misclassifications > 1"),3))
 prefix<- "D366-"
 suffix<- "_avg.csv"
 
