cc5b96e2d0ccc27b16e34b65e5bfcd38639e44e2
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2021-12-10 09:32:45 -0700

    12-10-21

--- Analysis/Merging_results.R
+++ Analysis/Merging_results.R
@@ -3,22 +3,26 @@ library(stringr)
 setwd("/n/home13/econsidine")
 
 Metrics<- c(rep(c("MAE", "RMSE"),3),
-            rep(c("Real class > shown class","Shown class > real class",
-                  "AQI class off by >1"),3),
+            rep(c("Real class > shown class","Shown class > real class"),3),
+            rep("AQI class off by >1",3),
             rep(c("95th percentile of errors",
                   "% of the |errors| > 10"),3),
-           rep(c("Mean distance to NN monitor/sensor"),3),
+            rep(c("Mean distance to NN monitor/sensor"),3),
             rep(c("Mean distance to NN monitor/sensor, among misclassifications > 1"),3),
             rep(c("Median distance to NN monitor/sensor"),3),
             rep(c("Median distance to NN monitor/sensor, among misclassifications > 1"),3),
             rep(c("Mean % of NNs that are LCSs"),3),
-            rep(c("Mean % of NNs that are LCSs, among misclassifications > 1"),3))
+            rep(c("Mean % of NNs that are LCSs, among misclassifications > 1"),3),
+            rep("Mean distance to NN monitor/sensor, among all misclassifications",3),
+            rep("Median distance to NN monitor/sensor, among all misclassifications",3),
+            rep("Mean % of NNs that are LCSs, among all misclassifications",3),
+            rep("% Unhealthy and Showing Healthy",3))
 prefix<- "D366-"
 suffix<- "_avg.csv"
 
-pre_files<- list.files("LCS_results",
+pre_files<- list.files("New_LCS_results",
            prefix)
-suff_files<- list.files("LCS_results",
+suff_files<- list.files("New_LCS_results",
                         suffix)
 files<- intersect(pre_files, suff_files)
 
@@ -29,7 +33,7 @@ unweighted<- str_detect(files, "unw")
 UNW_names<- sapply(files[unweighted], function(x) str_split(x, "-")[[1]][2])
 UNW_Names<- sapply(UNW_names, function(x) paste0(str_split(x, "0_")[[1]][1], "0"))
 
-UNW<- matrix(0, nrow=39, ncol=25)
+UNW<- matrix(0, nrow=51, ncol=25)
 
 for(i in 1:25){
   results<- read.csv(files[unweighted][i])
@@ -39,7 +43,7 @@ for(i in 1:25){
 colnames(UNW)<- UNW_Names
 row.names(UNW)<- Metrics
 
-write.csv(UNW, "LCS_results/Results_366_days_unweighted.csv")
+write.csv(UNW, "New_LCS_results/Results_366_days_unweighted.csv")
 
 
 # Get weighted
@@ -50,7 +54,7 @@ W_names<- sapply(files[weighted], function(x) str_split(x, "-")[[1]][2])
 W_Names<- sapply(W_names, function(x) paste0(str_split(x, "0_")[[1]][1], "0"))
 
 
-W<- matrix(0, nrow=39, ncol=25)
+W<- matrix(0, nrow=51, ncol=25)
 
 for(i in 1:25){
   results<- read.csv(files[weighted][i])
@@ -60,5 +64,79 @@ for(i in 1:25){
 colnames(W)<- W_Names
 row.names(W)<- Metrics
 
-write.csv(W, "LCS_results/Results_366_days_weighted.csv")
+write.csv(W, "New_LCS_results/Results_366_days_weighted.csv")
 
+#####################################################################
+############ Putting everything together to prepare for Shiny:
+
+pdw<- read.csv("New_LCS_results/Results_366_days_weighted.csv")
+unw<- read.csv("New_LCS_results/Results_366_days_unweighted.csv")
+
+names(pdw)[1]<- "X"
+names(unw)[1]<- "X"
+
+Metrics<- c(rep(c("MAE", "RMSE"),3),
+            rep(c("Real class > shown class","Shown class > real class"),3),
+            rep("AQI class off by >1",3),
+            rep(c("95th percentile of errors",
+                  "% of the |errors| > 10"),3),
+            rep(c("Mean distance to NN monitor/sensor"),3),
+            rep(c("Mean distance to NN monitor/sensor, among misclassifications > 1"),3),
+            rep(c("Median distance to NN monitor/sensor"),3),
+            rep(c("Median distance to NN monitor/sensor, among misclassifications > 1"),3),
+            rep(c("Mean % of NNs that are LCSs"),3),
+            rep(c("Mean % of NNs that are LCSs, among misclassifications > 1"),3),
+            rep("Mean distance to NN monitor/sensor, among all misclassifications",3),
+            rep("Median distance to NN monitor/sensor, among all misclassifications",3),
+            rep("Mean % of NNs that are LCSs, among all misclassifications",3),
+            rep("% Unhealthy and Showing Healthy",3))
+
+pdw[,1]<- Metrics
+unw[,1]<- Metrics
+
+## Add in AQS-only results
+
+aqs_w<- read.csv("New_LCS_results/D366-AQS_sites.csv")
+aqs_unw<- read.csv("New_LCS_results/D366-AQS_sites_unweighted.csv")
+
+pdw$AQS<- as.numeric(unlist(aqs_w))
+unw$AQS<- as.numeric(unlist(aqs_unw))
+
+pdw$AQS[c(34:39,46:48)]<- 0 # no LCSs for AQS only -- just showing collocations
+unw$AQS[c(34:39,46:48)]<- 0 # no LCSs for AQS only -- just showing collocations
+
+## Calculate % Unhealthy Showing Healthy / % Unhealthy
+
+unhealthy_w<- c(0.001863175, 0.00246099, 0.004137348)
+unhealthy_unw<- c(0.000612631, 0.00166163, 0.001190286)
+
+Names<- names(pdw)
+names(pdw)<- NULL
+names(unw)<- NULL
+
+pdw<- rbind(pdw, 0,0,0)
+pdw[52:54,1]<- "% Showing Healthy, out of Unhealthy"
+pdw[52,2:27]<- as.numeric(pdw[49,2:27])/unhealthy_w[1]
+pdw[53,2:27]<- as.numeric(pdw[50,2:27])/unhealthy_w[2]
+pdw[54,2:27]<- as.numeric(pdw[51,2:27])/unhealthy_w[3]
+
+unw<- rbind(unw, 0,0,0)
+unw[52:54,1]<- "% Showing Healthy, out of Unhealthy"
+unw[52,2:27]<- as.numeric(unw[49,2:27])/unhealthy_unw[1]
+unw[53,2:27]<- as.numeric(unw[50,2:27])/unhealthy_unw[2]
+unw[54,2:27]<- as.numeric(unw[51,2:27])/unhealthy_unw[3]
+
+names(pdw)<- Names
+names(unw)<- Names
+
+write.csv(pdw, "New_LCS_results/LCS_final_results_12-10-21_weighted.csv", row.names=FALSE)
+write.csv(unw, "New_LCS_results/LCS_final_results_12-10-21_unweighted.csv", row.names=FALSE)
+                 
+#################################
+
+### All PA sites:
+pdw<- read.csv("New_LCS_results/D366-All_PA_sites.csv")
+unw<- read.csv("New_LCS_results/D366-All_PA_sites_unweighted.csv")
+
+DF<- data.frame(Metrics, PDW=pdw, UNW=unw)
+write.csv(DF, "All_PA_sites_results_12-10-21.csv", row.names=FALSE)
