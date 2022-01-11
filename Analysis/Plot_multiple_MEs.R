ea7574acc68869e4c208778b8f4f082531530839
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2022-01-11 11:12:01 -0700

    Commenting

--- Analysis/Plot_multiple_MEs.R
+++ Analysis/Plot_multiple_MEs.R
@@ -1,3 +1,5 @@
+### Generate plots for manuscript
+
 library(ggplot2)
 library(cowplot)
 
@@ -5,6 +7,7 @@ setwd("C:/Users/ellen/OneDrive/MyDocs/Graduate Research/Low-cost AQ sensor epi")
 
 CBF<- palette.colors()
 
+## Function for generating each plot (from a specific simulation):
 my_plot<- function(df, j, t, units = "", ylab = NULL){
   df<- df[order(df$X),]
   k<- 3*(j-1)+1
@@ -68,7 +71,8 @@ my_plot<- function(df, j, t, units = "", ylab = NULL){
 }
 
 
-### MAE and distance: five ME variations and distance to NN -- 3x2, equal sized
+### MAE and distance plots: five ME variations and distance to NN -- 3x2, equal sized
+
 d<- my_plot(df = read.csv("Analysis/Results/Clsad results/SA-clsad-025-LCS_final_results_12-23-21_weighted.csv"),
             j = 10, t = "Distance to Nearest Monitor or Sensor", ylab = "Distance (m)")
 m1<- my_plot(df = read.csv("Analysis/Results/Sensitivity Analysis/LCS_final_results_SA_weighted.csv"),
@@ -106,7 +110,7 @@ plot_grid(Mrow, legend, ncol = 1, rel_heights = c(4, 0.5))
 
 dev.off()
 
-### UH Misclassifications: five ME variations -- no ME big across top, then 2x2 for rest
+### UH Misclassifications plots: five ME variations -- no ME big across top, then 2x2 for rest
 
 uh1<- my_plot(df = read.csv("Analysis/Results/Sensitivity Analysis/LCS_final_results_SA_weighted.csv"),
              j = 2, t = "No Sensor Measurement Error: UHM", ylab = "UH Misclassifications (%)")
@@ -137,6 +141,38 @@ plot_grid(uh1 + theme(legend.position="none"), Hrow,
 dev.off()
 
 
+## Distance to NN among misclass. > 1 plots:
+
+dm1<- my_plot(df = read.csv("Analysis/Results/Sensitivity Analysis/LCS_final_results_SA_weighted.csv"),
+              j = 12, t = "No Sensor ME: Distance Among Misclass. > 1", ylab = "Avg. Distance (m)")
+dm2<- my_plot(df = read.csv("Analysis/Results/Clsad results/SA-clsad-010-LCS_final_results_12-23-21_weighted.csv"),
+              j = 12, t = "10% Non-differential ME: Distance Among Misclass. > 1", ylab = "Avg. Distance (m)")
+dm3<- my_plot(df = read.csv("Analysis/Results/Clsad results/SA-clsad-025-LCS_final_results_12-23-21_weighted.csv"),
+              j = 12, t = "25% Non-differential ME: Distance Among Misclass. > 1", ylab = "Avg. Distance (m)")
+dm4<- my_plot(df = read.csv("Analysis/Results/New_results/SA-010-LCS_final_results_12-23-21_weighted.csv"),
+              j = 12, t = "10% Differential ME: Distance Among Misclass. > 1", ylab = "Avg. Distance (m)")
+dm5<- my_plot(df = read.csv("Analysis/Results/New_results/SA-025-LCS_final_results_12-10-21_weighted.csv"),
+              j = 12, t = "25% Differential ME: Distance Among Misclass. > 1", ylab = "Avg. Distance (m)")
+dm6<- my_plot(df = read.csv("Analysis/Results/New_results/LCS_final_results_12-10-21_weighted.csv"),
+              j = 12, t = "EPA Calibration Residuals: Distance Among Misclass. > 1", ylab = "Avg. Distance (m)")
+
+DMrow<- plot_grid(dm1 + theme(legend.position="none"),
+                 dm2 + theme(legend.position="none"),
+                 dm3 + theme(legend.position="none"),
+                 dm4 + theme(legend.position="none"),
+                 dm5 + theme(legend.position="none"),
+                 dm6 + theme(legend.position="none"),
+                 nrow = 3)
+
+png("Final Plots/Dist-Large-Misclass_weighted.png", width = 795, height = 645)
+
+legend<- get_legend(dm1 + theme(legend.position = "bottom", legend.box = "vertical",
+                                legend.key.width = unit(2, "line"),
+                                legend.spacing.y = unit(0.1, "cm")))
+
+plot_grid(DMrow, legend, ncol = 1, rel_heights = c(4, 0.5))
+
+dev.off()
 
 
 
